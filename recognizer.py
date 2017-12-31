2edfff753f781c9b990721554f65dc6bc620e4e3
daring-board <mahotox101500@gmail.com>
2018-01-01 02:17:50 +0900

    追加

--- recognizer.py
+++ recognizer.py
@@ -5,10 +5,20 @@ from time import sleep
 from subprocess import Popen
 
 class LangEngine:
+    queue = []
+
+    def createTmp(self, line):
+        tmp = 'tmp'
+        txt_path = 'speaker/txt/%s.txt'%tmp
+        line = line.split(':')[1]
+        with open(txt_path, 'w', encoding='sjis') as f:
+            f.write(line+'\n')
+        return tmp
+
     def listen(self):
         #julius -C main.jconf -dnnconf main.dnnconf
         cmd = 'julius_int\\julius.exe -C julius_int/main.jconf -dnnconf julius_int/main.dnnconf'
-        proc = Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        proc = Popen(cmd, stdout=subprocess.PIPE)
         count = 0
         while True:
             line = proc.stdout.readline()
@@ -18,10 +28,15 @@ class LangEngine:
                 count += 1
             if 'pass1_best:' in line:
                 line = line.replace(' ', '')
-                print(line)
                 if 'プロセスを終了' in line: break
+                if len(line.split(':')[1].strip()) == 0: continue
+                print(line)
+                self.speak(self.createTmp(line))
             if not line and proc.poll() is not None:
                 break
+            print(line)
+        print(line)
+        self.speak(self.createTmp(':システムを終了します。'))
         proc.kill()
 
     def speak(self, txt):
@@ -33,7 +48,6 @@ class LangEngine:
         cmd += '-x %s/dic -ow voice/%s.wav %s/%s.txt'%(root_path, txt, txt_path, txt)
         proc = Popen(cmd, stdout=subprocess.PIPE)
         proc.wait()
-        print('\nvoice/%s.wav'%txt)
         ao.output('voice/%s.wav'%txt)
 
 if __name__=='__main__':
