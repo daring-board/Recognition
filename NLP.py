c1075d629b7518287231b8c9bb2764b1204b03ed
daring-board <mahotox101500@gmail.com>
2018-01-12 05:46:19 +0900

    word2vecのコードを追加

--- NLP.py
+++ NLP.py
@@ -1,5 +1,7 @@
 import re
 from janome.tokenizer import Tokenizer
+from gensim.models import KeyedVectors
+from gensim.models import word2vec
 
 class NLP:
     '''
@@ -8,6 +10,8 @@ class NLP:
     def __init__(self):
         """コンストラクタ"""
         self.TOKENIZER = Tokenizer()
+#        self.w2v = KeyedVectors.load_word2vec_format('./brain/vocab/entity_vector.model.bin', binary=True)
+        self.w2v = word2vec.Word2Vec.load('./brain/vocab/word2vec.gensim.model')
 
     def analyze(self, text):
         """文字列textを形態素解析し、[(surface, parts)]の形にして返す。"""
@@ -17,3 +21,17 @@ class NLP:
     def is_keyword(self, part):
         """品詞partが学習すべきキーワードであるかどうかを真偽値で返す。"""
         return bool(re.match(r'名詞,(一般|代名詞|固有名詞|サ変接続|形容動詞語幹)', part))
+
+    def check(self):
+        similar_rugby_list = self.w2v.most_similar(u'ラグビー')
+        for similar_set in similar_rugby_list:
+            print(similar_set[0])
+            print(similar_set[1])
+        similar_rugby_list = self.w2v.most_similar(u'数学')
+        for similar_set in similar_rugby_list:
+            print(similar_set[0])
+            print(similar_set[1])
+
+if __name__ == '__main__':
+    nlp = NLP()
+    nlp.check()
